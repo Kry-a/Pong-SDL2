a194d400777da502523660db3fbb9d8f2cef31d2
KrasnotR <krasnogorskotr@gmail.com>
2020-01-26 19:15:11 -0300

    Added ball class and base functions

---
+++ Ball.cpp
@@ -0,0 +1,13 @@
+//
+// Created by krasno on 2020-01-26.
+//
+
+#include "Ball.h"
+
+void Ball::Bounce() {
+
+}
+
+void Ball::Update() {
+
+}
---
+++ Ball.h
@@ -0,0 +1,25 @@
+//
+// Created by krasno on 2020-01-26.
+//
+
+#ifndef PONG_BALL_H
+#define PONG_BALL_H
+
+
+#include <SDL_rect.h>
+
+class Ball {
+public:
+    float velocityX;
+    float velocityY;
+    float radius;
+    float xPos;
+    float yPos;
+
+public:
+    void Bounce();
+    void Update();
+};
+
+
+#endif //PONG_BALL_H
