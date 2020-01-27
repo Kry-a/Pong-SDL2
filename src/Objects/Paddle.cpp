a4466ed32cda548a7cf011c8ac7cebc3c335c7bc
KrasnotR <46050962+krasnotr@users.noreply.github.com>
2020-01-26 22:32:45 -0300

    Added documentation to paddle

--- src/Objects/Paddle.cpp
+++ src/Objects/Paddle.cpp
@@ -5,11 +5,20 @@
 #include "Paddle.h"
 #include "../Utils.h"
 
+/**
+ * Renders the paddle to the target @see SDL_Renderer
+ * @param pRenderer The target renderer
+ */
 void Paddle::Render(SDL_Renderer *pRenderer) {
     Utils::SetDrawColour(pRenderer, colour);
     SDL_RenderFillRect(pRenderer, GetCurrentRect());
 }
 
+/**
+ * Constructs a paddle
+ * @param x The X position of the left top of the paddle
+ * @param y The Y position of the left top of the paddle
+ */
 Paddle::Paddle(float x, float y) {
     xPos = x;
     yPos = y;
@@ -19,6 +28,10 @@ Paddle::Paddle(float x, float y) {
     rect.h = 32;
 }
 
+/**
+ * Updates X and Y values and returns the current @see SDL_Rect
+ * @return
+ */
 SDL_Rect *Paddle::GetCurrentRect() {
     rect.x = (int)xPos;
     rect.y = (int)yPos;
